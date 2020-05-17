9cb8f4e896135c0ac961e3c755bfa3d78d9ea85a
adamciecko1992 <adam.ciecko@hotmail.com>
2020-05-17 22:32:48 +0200

    styles

--- src/components/browser_form/browserFrom.js
+++ src/components/browser_form/browserFrom.js
@@ -1,157 +1,165 @@
 import template from "./browserForm.html";
 import { optionsCreation } from "../../services/optionsCreation/optionsCreation";
 import { convertMonth } from "../../services/convertMonth/convertMonth";
-import { monthToNum } from "../../services/monthToNum/monthToNum"
-import { calculateDays } from "../../services/calcDaysInMonth/calculateDays"
-
+import { monthToNum } from "../../services/monthToNum/monthToNum";
+import { calculateDays } from "../../services/calcDaysInMonth/calculateDays";
 
 class BrowserForm_model {
-    constructor() {
-        this.today = {};
-        this.getTodayObj();
-        this.dateIsValid = true;
+  constructor() {
+    this.today = {};
+    this.getTodayObj();
+    this.dateIsValid = true;
+  }
+  dateValidation(chosenDay, chosenMonth, chosenYear) {
+    const currentDay = this.today.day;
+    const currentMonthNum = monthToNum(this.today.month);
+    const currentYear = this.today.year;
+    const chosenMonthNum = monthToNum(chosenMonth);
+
+    if (
+      chosenDay < currentDay &&
+      chosenMonthNum <= currentMonthNum &&
+      chosenYear <= currentYear
+    ) {
+      return false;
     }
-    dateValidation(chosenDay, chosenMonth, chosenYear) {
-        const currentDay = this.today.day;
-        const currentMonthNum = monthToNum(this.today.month);
-        const currentYear = this.today.year;
-        const chosenMonthNum = monthToNum(chosenMonth);
-
-
-        if ((chosenDay < currentDay) &&
-            (chosenMonthNum <= currentMonthNum) &&
-            (chosenYear <= currentYear)) {
-
-            return false
-        }
-        if (chosenDay == currentDay &&
-            chosenMonthNum < currentMonthNum &&
-            chosenYear <= currentYear) {
-
-            return false
-        }
-        if ((chosenYear > currentYear) &&
-            (chosenMonthNum >= currentMonthNum) &&
-            (chosenDay > currentDay)) {
-
-            return false
-        }
-        if ((chosenYear > currentYear) &&
-            (chosenMonthNum > currentMonthNum)) {
-
-            return false
-        }
-        return true;
+    if (
+      chosenDay == currentDay &&
+      chosenMonthNum < currentMonthNum &&
+      chosenYear <= currentYear
+    ) {
+      return false;
     }
-    getTodayObj() {
-        const date = new Date()
-        this.today.day = date.getDate();
-        this.today.month = convertMonth(date.getMonth());
-        this.today.year = date.getFullYear();
-        this.today.days = calculateDays(this.today);
+    if (
+      chosenYear > currentYear &&
+      chosenMonthNum >= currentMonthNum &&
+      chosenDay > currentDay
+    ) {
+      return false;
     }
+    if (chosenYear > currentYear && chosenMonthNum > currentMonthNum) {
+      return false;
+    }
+    return true;
+  }
+  getTodayObj() {
+    const date = new Date();
+    this.today.day = date.getDate();
+    this.today.month = convertMonth(date.getMonth());
+    this.today.year = date.getFullYear();
+    this.today.days = calculateDays(this.today);
+  }
 }
-
 
 class BrowserForm_controller {
-    constructor(model, view, root_element) {
-        this.model = model;
-        this.view = view;
-        this.view.bind_searchClick(this.searchHandler.bind(this));
-        this.view.bind_monthChange(this.monthChangeHandler.bind(this));
-        this.view.bind_yearChange(this.yearChangeHandler.bind(this))
-        this.view.appendMarkup(root_element);
-        this.view.fillDataFiled(this.model.today);
+  constructor(model, view, root_element) {
+    this.model = model;
+    this.view = view;
+    this.view.bind_searchClick(this.searchHandler.bind(this));
+    this.view.bind_monthChange(this.monthChangeHandler.bind(this));
+    this.view.bind_yearChange(this.yearChangeHandler.bind(this));
+    this.view.appendMarkup(root_element);
+    this.view.fillDataFiled(this.model.today);
+  }
+  searchHandler(formData) {
+    if (
+      this.model.dateValidation(
+        this.view.depDateDay.value,
+        this.view.depDateMonth.value,
+        this.view.depDateYear.value
+      )
+    ) {
+      window.eventBus.dispatchEvent("search_triggered", formData);
     }
-    searchHandler(formData) {
-        if (this.model.dateValidation(this.view.depDateDay.value, this.view.depDateMonth.value, this.view.depDateYear.value)) {
-            window.eventBus.dispatchEvent("search_triggered", formData);
-        }
-    }
-    monthChangeHandler(newDate) {
-        this.view.fillDataFiled(newDate, true);
-    }
-    yearChangeHandler(newDate) {
-        // this.view.fillDataFiled(this.model.today, true);
-    }
+  }
+  monthChangeHandler(newDate) {
+    this.view.fillDataFiled(newDate, true);
+  }
+  yearChangeHandler(newDate) {
+    console.log(newDate);
+    this.view.fillDataFiled(newDate, true);
+  }
 }
 
-
-
 class BrowserForm_view {
-    constructor(target) {
-        this.markup = document.createRange().createContextualFragment(template);
-        this.box = this.markup.querySelector("#browserBox")
-        this.departurePort = this.markup.getElementById("departure")
-        this.arrivalPort = this.markup.getElementById("arrival")
-        this.depDateDay = this.markup.getElementById("dateDay")
-        this.depDateMonth = this.markup.querySelector("#dateMonth")
-        this.depDateYear = this.markup.getElementById("dateYear")
-        this.personCount = this.markup.getElementById("personCount")
-        this.searhButton = this.markup.querySelector("#searchButton")
-    }
+  constructor(target) {
+    this.markup = document.createRange().createContextualFragment(template);
+    this.box = this.markup.querySelector("#browserBox");
+    this.departurePort = this.markup.getElementById("departure");
+    this.arrivalPort = this.markup.getElementById("arrival");
+    this.depDateDay = this.markup.getElementById("dateDay");
+    this.depDateMonth = this.markup.querySelector("#dateMonth");
+    this.depDateYear = this.markup.getElementById("dateYear");
+    this.personCount = this.markup.getElementById("personCount");
+    this.searhButton = this.markup.querySelector("#searchButton");
+  }
 
-    bind_searchClick(handler) {
-        this.searhButton.addEventListener("click", (e) => {
-            e.preventDefault();
-            // if (sessionStorage.loggedIn === "true") { //turned off for development
-            const formData = {
-                departurePort: this.departurePort.value,
-                arrivalPort: this.arrivalPort.value,
-                depDateDay: this.depDateDay.value,
-                depDateMonth: this.depDateMonth.value,
-                depDateYear: this.depDateYear.value,
-                personCount: this.personCount.value
-            }
-            handler(formData);
-            // } else {
-            // alert("You have to be logged in to begin reservation process");
-            // }
-        })
-    }
-    bind_monthChange(handler) {
-        this.depDateMonth.addEventListener("input", () => {
-            const { depDateMonth, depDateDay, depDateYear } = this;
-            const newDate = {
-                day: depDateDay.value,
-                month: depDateMonth.value,
-                year: depDateYear.value,
-                days: null
-            }
-            newDate.days = calculateDays(newDate)
-            handler(newDate);
-        })
-    }
-    bind_yearChange(handler) {
-        this.depDateYear.addEventListener("input", () => {
-            const { depDateMonth, depDateDay, depDateYear } = this;
-            const newDate = {
-                day: depDateDay.value,
-                month: depDateMonth.value,
-                year: depDateYear.value,
-                days: null
-            }
-            newDate.days = calculateDays(newDate);
-            handler(this.depDateYear.value);
-        })
-    }
+  bind_searchClick(handler) {
+    this.searhButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      // if (sessionStorage.loggedIn === "true") { //turned off for development
+      const formData = {
+        departurePort: this.departurePort.value,
+        arrivalPort: this.arrivalPort.value,
+        depDateDay: this.depDateDay.value,
+        depDateMonth: this.depDateMonth.value,
+        depDateYear: this.depDateYear.value,
+        personCount: this.personCount.value,
+      };
+      handler(formData);
+      // } else {
+      // alert("You have to be logged in to begin reservation process");
+      // }
+    });
+  }
+  bind_monthChange(handler) {
+    this.depDateMonth.addEventListener("input", () => {
+      const { depDateMonth, depDateDay, depDateYear } = this;
+      const newDate = {
+        day: depDateDay.value,
+        month: depDateMonth.value,
+        year: depDateYear.value,
+        days: null,
+      };
+      newDate.days = calculateDays(newDate);
+      handler(newDate);
+    });
+  }
+  bind_yearChange(handler) {
+    this.depDateYear.addEventListener("input", () => {
+      const { depDateMonth, depDateDay, depDateYear } = this;
+      const newDate = {
+        day: depDateDay.value,
+        month: depDateMonth.value,
+        year: depDateYear.value,
+        days: null,
+      };
+      newDate.days = calculateDays(newDate);
+      handler(newDate);
+    });
+  }
 
-    fillDataFiled(dateObj) {
-        const selectedOptionIndex = dateObj.day - 1;
-        this.depDateDay.innerHTML = "";
-        optionsCreation(this.depDateDay, dateObj.days);
-        this.depDateMonth.value = dateObj.month;
-        this.depDateYear.value = dateObj.year;
-        const dayOptions = this.depDateDay.childNodes;
-        dayOptions[selectedOptionIndex].selected = true;
-    }
-    appendMarkup(target) {
-        target.appendChild(this.markup);
-    }
+  fillDataFiled(dateObj) {
+    console.log(dateObj);
+    const selectedOptionIndex = dateObj.day - 1;
+    this.depDateDay.innerHTML = "";
+    optionsCreation(this.depDateDay, dateObj.days);
+    this.depDateMonth.value = dateObj.month;
+    this.depDateYear.value = dateObj.year;
+    const dayOptions = this.depDateDay.childNodes;
+    dayOptions[selectedOptionIndex].selected = true;
+  }
+  appendMarkup(target) {
+    target.appendChild(this.markup);
+  }
 }
 
 export class BrowserForm {
-    constructor(root_element) {
-        new BrowserForm_controller(new BrowserForm_model, new BrowserForm_view(), root_element);
-    }
+  constructor(root_element) {
+    new BrowserForm_controller(
+      new BrowserForm_model(),
+      new BrowserForm_view(),
+      root_element
+    );
+  }
 }
--- src/components/reservationWindow/luggage_window/luggageWindow.js
+++ src/components/reservationWindow/luggage_window/luggageWindow.js
@@ -2,111 +2,123 @@ import template from "./luggageWindow.html";
 import { elementCreation } from "../../../services/elementCreation/elementCreation";
 import { appendChildren } from "../../../services/appendChildren/appendChildren";
 
-
-
 class LuggageWindow_model {
-    constructor(flightData) {
-        this.flightData = flightData;
-        this.flightData.luggageSelections = [];
-        this.flightData.airfareSelections = [];
-        this.persons = flightData.persons;
-        this.luggageTypes = ["small", "medium", "big"];
-        this.airfareTypes = ["economic", "buisnes", "premium"];
-    }
+  constructor(flightData) {
+    this.flightData = flightData;
+    this.flightData.luggageSelections = [];
+    this.flightData.airfareSelections = [];
+    this.persons = flightData.persons;
+    this.luggageTypes = ["small", "medium", "big"];
+    this.airfareTypes = ["economic", "buisnes", "premium"];
+  }
 }
 
 class LuggageWindow_controller {
-    constructor(model, view, root_element) {
-        this.parent = root_element;
-        this.model = model;
-        this.view = view;
+  constructor(model, view, root_element) {
+    this.parent = root_element;
+    this.model = model;
+    this.view = view;
 
-        this.handleReservationBtn = () => {
-            this.collectLuggageAndAirfare();
-            eventBus.dispatchEvent("reservationBtn_clicked", this.model.flightData);
-        }
-        this.view.createChoiceBoxes(this.view.airfareOptionsRoot, this.model.persons, this.model.airfareTypes);
-        this.view.createChoiceBoxes(this.view.luggageOptionsRoot, this.model.persons, this.model.luggageTypes);
+    this.handleReservationBtn = () => {
+      this.collectLuggageAndAirfare();
+      eventBus.dispatchEvent("reservationBtn_clicked", this.model.flightData);
+    };
+    this.view.createChoiceBoxes(
+      this.view.airfareOptionsRoot,
+      this.model.persons,
+      this.model.airfareTypes
+    );
+    this.view.createChoiceBoxes(
+      this.view.luggageOptionsRoot,
+      this.model.persons,
+      this.model.luggageTypes
+    );
 
-        this.view.bind_reservationButtonClicked(this.handleReservationBtn);
-        this.view.appendMarkup(root_element);
-    }
-    collectLuggageAndAirfare() {
-        this.view.luggageSelections.forEach((luggageSelection) => {
-            this.model.flightData.luggageSelections.push({ person: luggageSelection.person.innerText, luggage: luggageSelection.luggage.value })
-        })
-        this.view.airfareSelections.forEach((airfareSelection) => {
-            this.model.flightData.airfareSelections.push({ person: airfareSelection.person.innerText, airfare: airfareSelection.airfare.value })
-        })
-
-    }
-
+    this.view.bind_reservationButtonClicked(this.handleReservationBtn);
+    this.view.appendMarkup(root_element);
+  }
+  collectLuggageAndAirfare() {
+    this.view.luggageSelections.forEach((luggageSelection) => {
+      this.model.flightData.luggageSelections.push({
+        person: luggageSelection.person.innerText,
+        luggage: luggageSelection.luggage.value,
+      });
+    });
+    this.view.airfareSelections.forEach((airfareSelection) => {
+      this.model.flightData.airfareSelections.push({
+        person: airfareSelection.person.innerText,
+        airfare: airfareSelection.airfare.value,
+      });
+    });
+  }
 }
 
 class LuggageWindow_view {
-    constructor() {
-        this.markup = document.createRange().createContextualFragment(template);
-        this.box = this.markup.querySelector("#luggageWindow");
-        this.seatsReservationButton = this.markup.querySelector("#seatsReservationButton");
-        this.luggageOptionsRoot = this.markup.querySelector("#personsLuggageRoot");
-        this.airfareOptionsRoot = this.markup.querySelector("#personsAirfareRoot");
-        this.luggageSelections = [];
-        this.airfareSelections = [];
+  constructor() {
+    this.markup = document.createRange().createContextualFragment(template);
+    this.box = this.markup.querySelector("#luggageWindow");
+    this.seatsReservationButton = this.markup.querySelector(
+      "#seatsReservationButton"
+    );
+    this.luggageOptionsRoot = this.markup.querySelector("#personsLuggageRoot");
+    this.airfareOptionsRoot = this.markup.querySelector("#personsAirfareRoot");
+    this.luggageSelections = [];
+    this.airfareSelections = [];
+  }
+  hide() {
+    this.box.classList.remove("slideInRight", "delay-1s");
+    this.box.classList.add("animated", "slideOutLeft");
+    setTimeout(() => {
+      this.box.style.display = "none";
+    }, 1000);
+  }
+  appendMarkup(root_element) {
+    root_element.appendChild(this.box);
+    this.box.classList.add("animated", "slideInRight", "delay-1s");
+  }
+
+  bind_reservationButtonClicked(handler) {
+    this.seatsReservationButton.addEventListener("click", () => {
+      handler();
+    });
+  }
+
+  createChoiceBoxes(parent, num, optionsArr) {
+    for (let i = 1; i <= num; i++) {
+      const optionBox = elementCreation("div");
+      const optionSelect = elementCreation("select");
+      optionSelect.id = `Person ${i}`;
+      for (let i = 0; i < optionsArr.length; i++) {
+        const option = elementCreation("option", "text");
+        option.innerText = optionsArr[i];
+        option.value = optionsArr[i];
+        optionSelect.appendChild(option);
+      }
+      const label = elementCreation("span", "text", "label");
+      label.innerText = `Person ${i}`;
+      appendChildren(optionBox, label, optionSelect);
+      parent.appendChild(optionBox);
+      if (optionsArr.indexOf("small") !== -1) {
+        this.luggageSelections.push({
+          person: label,
+          luggage: optionSelect,
+        });
+      } else {
+        this.airfareSelections.push({
+          person: label,
+          airfare: optionSelect,
+        });
+      }
     }
-    hide() {
-        this.box.classList.remove("slideInRight", "delay-1s");
-        this.box.classList.add("animated", "slideOutLeft");
-        setTimeout(() => {
-            this.box.style.display = "none";
-        }, 1000)
-    }
-    appendMarkup(root_element) {
-        root_element.appendChild(this.box);
-        this.box.classList.add("animated", "slideInRight", "delay-1s");
-    }
-
-    bind_reservationButtonClicked(handler) {
-        this.seatsReservationButton.addEventListener("click", () => {
-            handler()
-        })
-    }
-
-
-    createChoiceBoxes(parent, num, optionsArr) {
-        for (let i = 1; i <= num; i++) {
-            const optionBox = elementCreation("div");
-            const optionSelect = elementCreation("select");
-            optionSelect.id = `Person ${i}`;
-            for (let i = 0; i < optionsArr.length; i++) {
-                const option = elementCreation("option", "text");
-                option.innerText = optionsArr[i];
-                option.value = optionsArr[i];
-                optionSelect.appendChild(option);
-            }
-            const label = elementCreation("span", "text");
-            label.innerText = `Person ${i}`;
-            appendChildren(optionBox, label, optionSelect);
-            parent.appendChild(optionBox);
-            if (optionsArr.indexOf("small") !== -1) {
-                this.luggageSelections.push({
-                    person: label,
-                    luggage: optionSelect
-                });
-
-            } else {
-                this.airfareSelections.push({
-                    person: label,
-                    airfare: optionSelect
-                });
-
-            }
-        }
-
-    }
+  }
 }
 
 export class LuggageWindow {
-    constructor(root_element, flightData) {
-        this.controller = new LuggageWindow_controller(new LuggageWindow_model(flightData), new LuggageWindow_view(), root_element)
-    }
+  constructor(root_element, flightData) {
+    this.controller = new LuggageWindow_controller(
+      new LuggageWindow_model(flightData),
+      new LuggageWindow_view(),
+      root_element
+    );
+  }
 }
