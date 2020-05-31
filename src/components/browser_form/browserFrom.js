38d3f38a5c58ae29c0e8108c381cfa27f9224af5
adamciecko1992 <adam.ciecko@hotmail.com>
2020-05-31 12:08:57 +0200

    no node_modules

--- src/components/browser_form/browserFrom.js
+++ src/components/browser_form/browserFrom.js
@@ -6,170 +6,171 @@ import { calculateDays } from "../../services/calcDaysInMonth/calculateDays";
 import { Popup } from "../popup/popup";
 
 class BrowserForm_model {
-  constructor() {
-    this.today = {};
-    this.getTodayObj();
-    this.dateIsValid = true;
-  }
-  dateValidation(chosenDay, chosenMonth, chosenYear) {
-    const currentDay = this.today.day;
-    const currentMonthNum = monthToNum(this.today.month);
-    const currentYear = this.today.year;
-    const chosenMonthNum = monthToNum(chosenMonth);
+    constructor() {
+        this.today = {};
+        this.getTodayObj();
+        this.dateIsValid = true;
+    }
+    dateValidation(chosenDay, chosenMonth, chosenYear) {
+        const currentDay = this.today.day;
+        const currentMonthNum = monthToNum(this.today.month);
+        const currentYear = this.today.year;
+        const chosenMonthNum = monthToNum(chosenMonth);
 
-    if (
-      chosenDay < currentDay &&
-      chosenMonthNum <= currentMonthNum &&
-      chosenYear <= currentYear
-    ) {
-      new Popup("Chosen date is in the past");
-      return false;
+        if (
+            chosenDay < currentDay &&
+            chosenMonthNum <= currentMonthNum &&
+            chosenYear <= currentYear
+        ) {
+            new Popup("Chosen date is in the past");
+            return false;
+        }
+        if (
+            chosenDay == currentDay &&
+            chosenMonthNum < currentMonthNum &&
+            chosenYear <= currentYear
+        ) {
+            new Popup("Chosen date is in the past");
+            return false;
+        }
+        if (
+            chosenYear > currentYear &&
+            chosenMonthNum >= currentMonthNum &&
+            chosenDay > currentDay
+        ) {
+            new Popup(
+                "Chosen date is too far away",
+                "There is no posiibility to make reservation on flight which is more than a year away from today"
+            );
+            return false;
+        }
+        if (chosenYear > currentYear && chosenMonthNum > currentMonthNum) {
+            new Popup(
+                "Chosen date is too far away",
+                "There is no posiibility to make reservation on flight which is more than a year away from today"
+            );
+            return false;
+        }
+        return true;
     }
-    if (
-      chosenDay == currentDay &&
-      chosenMonthNum < currentMonthNum &&
-      chosenYear <= currentYear
-    ) {
-      new Popup("Chosen date is in the past");
-      return false;
+    getTodayObj() {
+        const date = new Date();
+        const { today } = this;
+        today.day = date.getDate();
+        today.month = convertMonth(date.getMonth());
+        today.year = date.getFullYear();
+        today.days = calculateDays(this.today);
     }
-    if (
-      chosenYear > currentYear &&
-      chosenMonthNum >= currentMonthNum &&
-      chosenDay > currentDay
-    ) {
-      new Popup(
-        "Chosen date is too far away",
-        "There is no posiibility to make reservation on flight which is more than a year away from today"
-      );
-      return false;
-    }
-    if (chosenYear > currentYear && chosenMonthNum > currentMonthNum) {
-      new Popup(
-        "Chosen date is too far away",
-        "There is no posiibility to make reservation on flight which is more than a year away from today"
-      );
-      return false;
-    }
-    return true;
-  }
-  getTodayObj() {
-    const date = new Date();
-    this.today.day = date.getDate();
-    this.today.month = convertMonth(date.getMonth());
-    this.today.year = date.getFullYear();
-    this.today.days = calculateDays(this.today);
-  }
 }
 
 class BrowserForm_controller {
-  constructor(model, view, root_element) {
-    this.model = model;
-    this.view = view;
-    this.view.bind_searchClick(this.searchHandler.bind(this));
-    this.view.bind_monthChange(this.monthChangeHandler.bind(this));
-    this.view.bind_yearChange(this.yearChangeHandler.bind(this));
-    this.view.appendMarkup(root_element);
-    this.view.fillDataFiled(this.model.today);
-  }
-  searchHandler(formData) {
-    if (
-      this.model.dateValidation(
-        this.view.depDateDay.value,
-        this.view.depDateMonth.value,
-        this.view.depDateYear.value
-      )
-    ) {
-      window.eventBus.dispatchEvent("search_triggered", formData);
+    constructor(model, view, root_element) {
+        this.model = model;
+        this.view = view;
+        this.view.bind_searchClick(this.searchHandler.bind(this));
+        this.view.bind_monthChange(this.monthChangeHandler.bind(this));
+        this.view.bind_yearChange(this.yearChangeHandler.bind(this));
+        this.view.appendMarkup(root_element);
+        this.view.fillDataFiled(this.model.today);
     }
-  }
-  monthChangeHandler(newDate) {
-    this.view.fillDataFiled(newDate, true);
-  }
-  yearChangeHandler(newDate) {
-    this.view.fillDataFiled(newDate, true);
-  }
+    searchHandler(formData) {
+        if (
+            this.model.dateValidation(
+                this.view.depDateDay.value,
+                this.view.depDateMonth.value,
+                this.view.depDateYear.value
+            )
+        ) {
+            window.eventBus.dispatchEvent("search_triggered", formData);
+        }
+    }
+    monthChangeHandler(newDate) {
+        this.view.fillDataFiled(newDate, true);
+    }
+    yearChangeHandler(newDate) {
+        this.view.fillDataFiled(newDate, true);
+    }
 }
 
 class BrowserForm_view {
-  constructor(target) {
-    this.markup = document.createRange().createContextualFragment(template);
-    this.box = this.markup.querySelector("#browserBox");
-    this.departurePort = this.markup.getElementById("departure");
-    this.arrivalPort = this.markup.getElementById("arrival");
-    this.depDateDay = this.markup.getElementById("dateDay");
-    this.depDateMonth = this.markup.querySelector("#dateMonth");
-    this.depDateYear = this.markup.getElementById("dateYear");
-    this.personCount = this.markup.getElementById("personCount");
-    this.searhButton = this.markup.querySelector("#searchButton");
-  }
+    constructor(target) {
+        this.markup = document.createRange().createContextualFragment(template);
+        this.box = this.markup.querySelector("#browserBox");
+        this.departurePort = this.markup.getElementById("departure");
+        this.arrivalPort = this.markup.getElementById("arrival");
+        this.depDateDay = this.markup.getElementById("dateDay");
+        this.depDateMonth = this.markup.querySelector("#dateMonth");
+        this.depDateYear = this.markup.getElementById("dateYear");
+        this.personCount = this.markup.getElementById("personCount");
+        this.searhButton = this.markup.querySelector("#searchButton");
+    }
 
-  bind_searchClick(handler) {
-    this.searhButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (sessionStorage.loggedIn === "true") {
-        //turned off for development
-        const formData = {
-          departurePort: this.departurePort.value,
-          arrivalPort: this.arrivalPort.value,
-          depDateDay: this.depDateDay.value,
-          depDateMonth: this.depDateMonth.value,
-          depDateYear: this.depDateYear.value,
-          personCount: this.personCount.value,
-        };
-        handler(formData);
-      } else {
-        new Popup("Log In", "Please log in to begin the search process");
-      }
-    });
-  }
-  bind_monthChange(handler) {
-    this.depDateMonth.addEventListener("input", () => {
-      const { depDateMonth, depDateDay, depDateYear } = this;
-      const newDate = {
-        day: depDateDay.value,
-        month: depDateMonth.value,
-        year: depDateYear.value,
-        days: null,
-      };
-      newDate.days = calculateDays(newDate);
-      handler(newDate);
-    });
-  }
-  bind_yearChange(handler) {
-    this.depDateYear.addEventListener("input", () => {
-      const { depDateMonth, depDateDay, depDateYear } = this;
-      const newDate = {
-        day: depDateDay.value,
-        month: depDateMonth.value,
-        year: depDateYear.value,
-        days: null,
-      };
-      newDate.days = calculateDays(newDate);
-      handler(newDate);
-    });
-  }
+    bind_searchClick(handler) {
+        this.searhButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (sessionStorage.loggedIn === "true") {
+                //turned off for development
+                const formData = {
+                    departurePort: this.departurePort.value,
+                    arrivalPort: this.arrivalPort.value,
+                    depDateDay: this.depDateDay.value,
+                    depDateMonth: this.depDateMonth.value,
+                    depDateYear: this.depDateYear.value,
+                    personCount: this.personCount.value,
+                };
+                handler(formData);
+            } else {
+                new Popup("Log In", "Please log in to begin the search process");
+            }
+        });
+    }
+    bind_monthChange(handler) {
+        this.depDateMonth.addEventListener("input", () => {
+            const { depDateMonth, depDateDay, depDateYear } = this;
+            const newDate = {
+                day: depDateDay.value,
+                month: depDateMonth.value,
+                year: depDateYear.value,
+                days: null,
+            };
+            newDate.days = calculateDays(newDate);
+            handler(newDate);
+        });
+    }
+    bind_yearChange(handler) {
+        this.depDateYear.addEventListener("input", () => {
+            const { depDateMonth, depDateDay, depDateYear } = this;
+            const newDate = {
+                day: depDateDay.value,
+                month: depDateMonth.value,
+                year: depDateYear.value,
+                days: null,
+            };
+            newDate.days = calculateDays(newDate);
+            handler(newDate);
+        });
+    }
 
-  fillDataFiled(dateObj) {
-    const selectedOptionIndex = dateObj.day - 1;
-    this.depDateDay.innerHTML = "";
-    optionsCreation(this.depDateDay, dateObj.days);
-    this.depDateMonth.value = dateObj.month;
-    this.depDateYear.value = dateObj.year;
-    const dayOptions = this.depDateDay.childNodes;
-    dayOptions[selectedOptionIndex].selected = true;
-  }
-  appendMarkup(target) {
-    target.appendChild(this.markup);
-  }
+    fillDataFiled(dateObj) {
+        const selectedOptionIndex = dateObj.day - 1;
+        this.depDateDay.innerHTML = "";
+        optionsCreation(this.depDateDay, dateObj.days);
+        this.depDateMonth.value = dateObj.month;
+        this.depDateYear.value = dateObj.year;
+        const dayOptions = this.depDateDay.childNodes;
+        dayOptions[selectedOptionIndex].selected = true;
+    }
+    appendMarkup(target) {
+        target.appendChild(this.markup);
+    }
 }
 
 export class BrowserForm {
-  constructor(root_element) {
-    new BrowserForm_controller(
-      new BrowserForm_model(),
-      new BrowserForm_view(),
-      root_element
-    );
-  }
+    constructor(root_element) {
+        new BrowserForm_controller(
+            new BrowserForm_model(),
+            new BrowserForm_view(),
+            root_element
+        );
+    }
 }
--- src/components/reservationWindow/seats_reservation_window/seatsReservation.js
+++ src/components/reservationWindow/seats_reservation_window/seatsReservation.js
@@ -1,223 +1,227 @@
 import {
-  bombardier_Svg,
-  boeing_747_Svg,
-  boeing_737_Svg,
+    bombardier_Svg,
+    boeing_747_Svg,
+    boeing_737_Svg,
 } from "./assets/aircraftSvg";
 import template from "./seatsReservationWindow.html";
 import { Popup } from "../../popup/popup";
 
 class SeatsReservation_model {
-  constructor(flightData) {
-    this.flightData = flightData;
-    this.svg = this.getMatchedImg(flightData.aircraft);
-    this.seatsRows = {};
-    this.chosenSeats = [];
-  }
-  getMatchedImg(aircraft) {
-    let svgPlane;
-    switch (aircraft) {
-      case "Bombardier":
-        svgPlane = bombardier_Svg;
-        break;
-      case "Boeing_737":
-        svgPlane = boeing_737_Svg;
-        break;
-      case "Boeing_747":
-        svgPlane = boeing_747_Svg;
+    constructor(flightData) {
+        this.flightData = flightData;
+        this.svg = this.getMatchedImg(flightData.aircraft);
+        this.seatsRows = {};
+        this.chosenSeats = [];
     }
-    return svgPlane;
-  }
-  sortSeatsByRow(seats) {
-    let seatsArr = [...seats];
-    seatsArr.forEach((seat, index, arr) => {
-      const svgNum = seat.attributes[0].value.slice(1, 4);
-      if (!this.seatsRows[svgNum]) {
-        this.seatsRows[svgNum] = [];
-        this.seatsRows[svgNum].push(seat);
-        const seatNum = arr.indexOf(seat);
-        arr.splice(seatNum, 1);
-      } else {
-        this.seatsRows[svgNum].push(seat);
-        const seatNum = arr.indexOf(seat);
-        arr.splice(seatNum, 1);
-      }
-    });
-    if (seatsArr.length > 0) {
-      this.sortSeatsByRow(seatsArr);
+    getMatchedImg(aircraft) {
+        let svgPlane;
+        switch (aircraft) {
+            case "Bombardier":
+                svgPlane = bombardier_Svg;
+                break;
+            case "Boeing_737":
+                svgPlane = boeing_737_Svg;
+                break;
+            case "Boeing_747":
+                svgPlane = boeing_747_Svg;
+        }
+        return svgPlane;
     }
-  }
-  sortSeatsInRow(seatsRows) {
-    const rows = Object.values(this.seatsRows);
-    function getH(seat) {
-      return seat.indexOf("H");
+    sortSeatsByRow(seats) {
+        let seatsArr = [...seats];
+        seatsArr.forEach((seat, index, arr) => {
+            const svgNum = seat.attributes[0].value.slice(1, 4);
+            if (!this.seatsRows[svgNum]) {
+                this.seatsRows[svgNum] = [];
+                this.seatsRows[svgNum].push(seat);
+                const seatNum = arr.indexOf(seat);
+                arr.splice(seatNum, 1);
+            } else {
+                this.seatsRows[svgNum].push(seat);
+                const seatNum = arr.indexOf(seat);
+                arr.splice(seatNum, 1);
+            }
+        });
+        if (seatsArr.length > 0) {
+            this.sortSeatsByRow(seatsArr);
+        }
     }
-    for (let row of rows) {
-      row.sort(
-        (a, b) =>
-          parseInt(
-            a.attributes[0].value.slice(4, getH(a.attributes[0].value))
-          ) -
-          parseInt(b.attributes[0].value.slice(4, getH(b.attributes[0].value)))
-      );
-    }
-  }
-  addNumberToSeats() {
-    const seats = Object.values(this.seatsRows);
-    const rows = Object.keys(this.seatsRows);
+    sortSeatsInRow(seatsRows) {
+        const rows = Object.values(this.seatsRows);
 
-    if (this.flightData.aircraft === "Boeing_737") {
-      const lastRows = seats.splice(0, 13);
-      seats.push(...lastRows);
+        function getH(seat) {
+            return seat.indexOf("H");
+        }
+        for (let row of rows) {
+            row.sort(
+                (a, b) => {
+                    return parseInt(
+                            a.attributes[0].value.slice(4, getH(a.attributes[0].value))
+                        ) -
+                        parseInt(b.attributes[0].value.slice(4, getH(b.attributes[0].value)))
+                });
+
+        }
     }
-    if (this.flightData.aircraft === "Boeing_747") {
-      const lastRows = seats.splice(0, 32);
-      seats.push(...lastRows);
+    addNumberToSeats() {
+        const seats = Object.values(this.seatsRows);
+        const rows = Object.keys(this.seatsRows);
+
+        if (this.flightData.aircraft === "Boeing_737") {
+            const lastRows = seats.splice(0, 13);
+            seats.push(...lastRows);
+        }
+        if (this.flightData.aircraft === "Boeing_747") {
+            const lastRows = seats.splice(0, 32);
+            seats.push(...lastRows);
+        }
+        for (let i = 0; i < rows.length; i++) {
+            for (let j = 0; j < seats[i].length; j++) {
+                seats[i][j].id = `Row_${i + 1}_Number_${j + 1}`;
+            }
+        }
     }
-    for (let i = 0; i < rows.length; i++) {
-      for (let j = 0; j < seats[i].length; j++) {
-        seats[i][j].id = `Row_${i + 1}_Number_${j + 1}`;
-      }
-    }
-  }
 }
 
 class SeatsReservation_controller {
-  constructor(model, view, root_element) {
-    this.model = model;
-    this.view = view;
-    this.view.showPlane(this.model.svg);
-    this.view.getSeatsList(this.model.flightData.aircraft);
-    this.model.sortSeatsByRow(this.view.seats);
-    this.model.sortSeatsInRow(this.model.seatsRows);
-    this.model.addNumberToSeats();
-    this.view.bind_seatClick(
-      this.handleSeatClick.bind(this),
-      this.handleSeatRemove.bind(this),
-      this.model.flightData.persons
-    );
-    this.view.bind_confirmClick(this.handleConfirmClick.bind(this));
-    this.view.appendMarkup(root_element);
-  }
-  handleSeatClick(clickedSeatNumber, currentPerson) {
-    this.model.chosenSeats.push({
-      Person: `${currentPerson}`,
-      seat: `${clickedSeatNumber}`,
-    });
-    this.view.currentPerson.style.color = "black";
-  }
-  handleSeatRemove(removedSeatNumber, currentPerson) {
-    const seatIndex = this.model.chosenSeats.indexOf({
-      Preson: `${currentPerson}`,
-      seat: `${removedSeatNumber}`,
-    });
-    this.model.chosenSeats.splice(seatIndex, 1);
-  }
-  handleConfirmClick() {
-    if (
-      this.model.chosenSeats.length === parseInt(this.model.flightData.persons)
-    ) {
-      this.model.flightData.chosenSeats = this.model.chosenSeats;
-      window.eventBus.dispatchEvent("seats_chosen", this.model.flightData);
-    } else {
-      // new Popup(`Not all passangers had chosen their seats`);
-      this.view.currentPerson.innerHTML =
-        "Not all passangers have chosen their seats";
-      this.view.currentPerson.style.color = "red";
+    constructor(model, view, root_element) {
+        this.model = model;
+        this.view = view;
+        this.view.showPlane(this.model.svg);
+        this.view.getSeatsList(this.model.flightData.aircraft);
+        this.model.sortSeatsByRow(this.view.seats);
+        this.model.sortSeatsInRow(this.model.seatsRows);
+        this.model.addNumberToSeats();
+        this.view.bind_seatClick(
+            this.handleSeatClick.bind(this),
+            this.handleSeatRemove.bind(this),
+            this.model.flightData.persons
+        );
+        this.view.bind_confirmClick(this.handleConfirmClick.bind(this));
+        this.view.appendMarkup(root_element);
     }
-  }
+    handleSeatClick(clickedSeatNumber, currentPerson) {
+        this.model.chosenSeats.push({
+            Person: `${currentPerson}`,
+            seat: `${clickedSeatNumber}`,
+        });
+        console.log(clickedSeatNumber);
+        this.view.currentPerson.style.color = "black";
+
+    }
+    handleSeatRemove(removedSeatNumber, currentPerson) {
+        const seatIndex = this.model.chosenSeats.indexOf({
+            Preson: `${currentPerson}`,
+            seat: `${removedSeatNumber}`,
+        });
+        this.model.chosenSeats.splice(seatIndex, 1);
+    }
+    handleConfirmClick() {
+        if (
+            this.model.chosenSeats.length === parseInt(this.model.flightData.persons)
+        ) {
+            this.model.flightData.chosenSeats = this.model.chosenSeats;
+            window.eventBus.dispatchEvent("seats_chosen", this.model.flightData);
+        } else {
+            // new Popup(`Not all passangers had chosen their seats`);
+            this.view.currentPerson.innerHTML =
+                "Not all passangers have chosen their seats";
+            this.view.currentPerson.style.color = "red";
+        }
+    }
 }
 
 class SeatsReservation_view {
-  constructor() {
-    this.markup = document.createRange().createContextualFragment(template);
-    this.box = this.markup.querySelector("#seatsReservationWindow");
-    this.svgRoot = this.markup.querySelector("#svgRoot");
-    this.currentPerson = this.markup.querySelector("#currentPerson");
-    this.confirmSeatsBtn = this.markup.querySelector("#confirmSeatsBtn");
-    this.currentPerson.innerHTML = "Person 1";
-    this.seats = [];
-  }
-  showPlane(plane) {
-    this.svgRoot.innerHTML = plane;
-  }
-  getSeatsList(aircraftSvg) {
-    const paths = this.markup.querySelectorAll("path");
-    const pathsArr = [...paths];
-    switch (aircraftSvg) {
-      case "Bombardier":
-        pathsArr.shift();
-        pathsArr.splice(60, 5);
-        pathsArr.pop();
-        this.seats = pathsArr;
-        break;
-      case "Boeing_737":
-        pathsArr.splice(0, 4);
-        pathsArr.splice(186, 2);
-        pathsArr.pop();
-        this.seats = pathsArr;
-        break;
-      case "Boeing_747":
-        pathsArr.splice(0, 4);
-        pathsArr.splice(279, 2);
-        pathsArr.splice(-3, 3);
-
-        this.seats = pathsArr;
-        break;
+    constructor() {
+        this.markup = document.createRange().createContextualFragment(template);
+        this.box = this.markup.querySelector("#seatsReservationWindow");
+        this.svgRoot = this.markup.querySelector("#svgRoot");
+        this.currentPerson = this.markup.querySelector("#currentPerson");
+        this.confirmSeatsBtn = this.markup.querySelector("#confirmSeatsBtn");
+        this.currentPerson.innerHTML = "Person 1";
+        this.seats = [];
     }
-  }
-  bind_seatClick(handleChoose, handleRemove, personCount) {
-    let availableSeats = personCount;
-    let currentPerson = 1;
-    for (let seat of this.seats) {
-      seat.addEventListener("click", (event) => {
-        if (event.target.classList.contains("active")) {
-          event.target.classList.remove("active");
-          availableSeats++;
-          currentPerson--;
-          this.currentPerson.innerHTML = `Person ${currentPerson}`;
-          handleRemove(event.target.id, currentPerson + 1);
-        } else {
-          if (availableSeats > 0) {
-            seat.classList.toggle("active");
-            availableSeats--;
-            currentPerson++;
-            this.currentPerson.innerHTML = `Person ${currentPerson}`;
-            if (availableSeats === 0) {
-              this.currentPerson.innerHTML = `Completed`;
-            }
-            handleChoose(event.target.id, currentPerson - 1);
-          } else {
-            alert("No more seats");
-          }
+    showPlane(plane) {
+        this.svgRoot.innerHTML = plane;
+    }
+    getSeatsList(aircraftSvg) {
+        const paths = this.markup.querySelectorAll("path");
+        const pathsArr = [...paths];
+        switch (aircraftSvg) {
+            case "Bombardier":
+                pathsArr.shift();
+                pathsArr.splice(60, 5);
+                pathsArr.pop();
+                this.seats = pathsArr;
+                break;
+            case "Boeing_737":
+                pathsArr.splice(0, 4);
+                pathsArr.splice(186, 2);
+                pathsArr.pop();
+                this.seats = pathsArr;
+                break;
+            case "Boeing_747":
+                pathsArr.splice(0, 4);
+                pathsArr.splice(279, 2);
+                pathsArr.splice(-3, 3);
+
+                this.seats = pathsArr;
+                break;
         }
-      });
     }
-  }
+    bind_seatClick(handleChoose, handleRemove, personCount) {
+        let availableSeats = personCount;
+        let currentPerson = 1;
+        for (let seat of this.seats) {
+            seat.addEventListener("click", (event) => {
+                if (event.target.classList.contains("active")) {
+                    event.target.classList.remove("active");
+                    availableSeats++;
+                    currentPerson--;
+                    this.currentPerson.innerHTML = `Person ${currentPerson}`;
+                    handleRemove(event.target.id, currentPerson + 1);
+                } else {
+                    if (availableSeats > 0) {
+                        seat.classList.toggle("active");
+                        availableSeats--;
+                        currentPerson++;
+                        this.currentPerson.innerHTML = `Person ${currentPerson}`;
+                        if (availableSeats === 0) {
+                            this.currentPerson.innerHTML = `Completed`;
+                        }
+                        handleChoose(event.target.id, currentPerson - 1);
+                    } else {
+                        alert("No more seats");
+                    }
+                }
+            });
+        }
+    }
 
-  bind_confirmClick(handler) {
-    this.confirmSeatsBtn.addEventListener("click", () => {
-      handler();
-    });
-  }
-  hide() {
-    this.box.classList.remove("slideInRight", "delay-1s");
-    this.box.classList.add("animated", "slideOutLeft");
-    setTimeout(() => {
-      this.box.style.display = "none";
-    }, 1000);
-  }
-  appendMarkup(root_element) {
-    root_element.appendChild(this.box);
-    this.box.classList.add("animated", "slideInRight", "delay-1s");
-  }
+    bind_confirmClick(handler) {
+        this.confirmSeatsBtn.addEventListener("click", () => {
+            handler();
+        });
+    }
+    hide() {
+        this.box.classList.remove("slideInRight", "delay-1s");
+        this.box.classList.add("animated", "slideOutLeft");
+        setTimeout(() => {
+            this.box.style.display = "none";
+        }, 1000);
+    }
+    appendMarkup(root_element) {
+        root_element.appendChild(this.box);
+        this.box.classList.add("animated", "slideInRight", "delay-1s");
+    }
 }
 
 export class SeatsReservation {
-  constructor(root_element, flightData) {
-    this.controller = new SeatsReservation_controller(
-      new SeatsReservation_model(flightData),
-      new SeatsReservation_view(),
-      root_element
-    );
-  }
+    constructor(root_element, flightData) {
+        this.controller = new SeatsReservation_controller(
+            new SeatsReservation_model(flightData),
+            new SeatsReservation_view(),
+            root_element
+        );
+    }
 }
